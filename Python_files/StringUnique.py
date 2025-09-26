97631f96ec41578d2b652baabf7f8ab023e36aa0
hamshik <hamshik.sbv3@gmail.com>
2025-09-26 21:17:00 +0530

    update

--- Python_files/StringUnique.py
+++ Python_files/StringUnique.py
@@ -1,22 +1,34 @@
-rawStr: str = input("Enter the letters: ")
-def solution(rawStr:str) -> int:
-    i:int = 0;
+def solution(rawStr:str) -> str:
+    k:int = 0;
     counter:int = 0
     rawList: list[str] = []
     lists: list[str] = []
     countList:list[int] = []
     rawList = [char for char in rawStr]
-    rawAns: str =  ""
-    for chars in range(len(rawList)):
-        for char in range(len(rawList)):
-            rawAns  = rawList[char] + rawList[chars]
-            if rawAns in lists:
-                countList.append(len(rawAns))  
-                rawAns = ""
-                lists.clear()
+    for d in rawList:
+        k  += 1
+        if (d not in lists):
+            lists.append(d)
+        else:
+            if(lists[0] == d):
+                lists.pop(0)
+                rawList.pop(0)
+                rawList.append(d)
             else:
-                lists.append(rawList[char])
+                countList.append(len(lists))
+                lists.clear()
+                lists.append(d)
+    if (rawList[len(rawList) - 1] not in lists):
+        lists.append(rawList[len(rawList) - 1])
+    countList.append(len(lists))
     countList.append(len(lists))
     counter = max(countList)
-    return counter
+    return f"The longest unique letter  of group is {lists} and its length is {counter}"
+
+while True:
+    rawStr: str = input("Enter the letters: ")
+    if not(rawStr == "" or rawStr.isalpha()):
+        print("invaild input")
+    else:
+        break
 print(solution(rawStr))
