d25ac49d67ff5832c1fb96a02a1d8f2d857b0924
Hamshik <114989431+Hamshik@users.noreply.github.com>
2025-10-12 04:20:20 +0000

    updated

--- JavaProject/src/main/java/in/hamshik/Currency.java
+++ JavaProject/src/main/java/in/hamshik/Currency.java
@@ -4,23 +4,21 @@ import java.text.*;
 public class Currency{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		double pay = in.nextDouble();
-		in.close();
-		Locale LocaleIn = Locale.forLanguageTag("en-IN");
-		NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
-		NumberFormat chiformat = NumberFormat.getCurrencyInstance(Locale.CHINA);
-		NumberFormat informat = NumberFormat.getCurrencyInstance(LocaleIn);
-		NumberFormat FraFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+		try{
+			double pay = in.nextDouble();
+			in.close();
+			Locale LocaleIn = Locale.forLanguageTag("en-IN");
+			String us = NumberFormat.getCurrencyInstance(Locale.US).format(pay);
+			String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(pay);
+			String india = NumberFormat.getCurrencyInstance(LocaleIn).format(pay);
+			String France = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(pay);
 
-		String india = informat.format(pay);
-		String china = chiformat.format(pay);
-		String us = usFormat.format(pay);
-		String france = FraFormat.format(pay);
-
-		System.out.println("US: " + us); 
-		System.out.println("India: " + india); 
-		System.out.println("China: " + china); 
-		System.out.println("France: " + france);
+			System.out.println("US: " + us);
+			System.out.println("India: " + india);
+			System.out.println("China: " + china);
+			System.out.println("France: " + France);
+		}
+		catch(Exception e){}
 		
 	}
 }
