279bf87f029822b99d2a9440d2d60ced3780876c
hamshik <hamshik.sbv3@gmail.com>
2025-10-20 17:06:53 +0530

    updated

--- JavaProject/coffee_eatter/src/main/java/in/hamshik/App.java
+++ JavaProject/coffee_eatter/src/main/java/in/hamshik/App.java
@@ -2,6 +2,7 @@ package in.hamshik;
 
 import java.io.IOException;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,21 +19,38 @@ public class App extends Application{
         try{
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Scene.fxml"));
             Parent root = loader.load();
-            Scene scene = new Scene(root);
+            Scene scene = new Scene(root,500,650);
             Image icon = new Image(getClass().getResourceAsStream("/coffee.png"));
-            stage.setWidth(500);
-            stage.setHeight(650);
             stage.setResizable(false);
             stage.getIcons().add(icon);
 
             Controller controller = loader.getController();
 
-            scene.setOnKeyPressed(e -> {
-                controller.keyHandler(e.getCode());
+            scene.setOnKeyPressed(event -> {
+                System.out.println("Key Pressed: " + event.getCode());
+                TranslateTransition transition = new TranslateTransition();
+                transition.setNode(controller.heroView);
+                transition.setDuration(javafx.util.Duration.millis(200));
+                switch (event.getCode()) {
+                    case LEFT, A:
+                        transition.setByX(-20);
+                        break;
+                    case RIGHT, D:
+                        transition.setByX(+20);
+                        break;
+                    default:
+                        return; // Ignore other keys.
+                
+                }
+                transition.play();
+
             });
+
             stage.setScene(scene);
             stage.setTitle("Coffee Bean Eater Game");
             stage.show();
+            scene.getRoot().requestFocus();
+            
 
         }
         catch(Exception e){
--- JavaProject/coffee_eatter/src/main/java/in/hamshik/Controller.java
+++ JavaProject/coffee_eatter/src/main/java/in/hamshik/Controller.java
@@ -1,21 +1,19 @@
 package in.hamshik;
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.Random;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.animation.TranslateTransition;
 
 
 public class Controller implements Initializable {
     Random random = new Random();
-    private KeyCode code;
     @FXML
     private ImageView coffee1View;
     @FXML
@@ -23,16 +21,46 @@ public class Controller implements Initializable {
     @FXML
     private ImageView coffee3View;
     @FXML
-    private ImageView villainView;
+    public ImageView villainView;
     @FXML
-    private ImageView heroView;
+    public ImageView heroView;
 
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1) throws  UnsupportedOperationException{
-        
-    }
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        Random random = new Random();
 
-    public void keyHandler(KeyCode code) {
-        this.code = code;
+// Start X anywhere in window width (0 to 450), leave some margin (50px cup width)
+coffee1View.setX(random.nextInt(450));
+coffee2View.setX(random.nextInt(450));
+coffee3View.setX(random.nextInt(450));
+
+// Set all Y positions to 0 (top of the window)
+coffee1View.setY(0);
+coffee2View.setY(0);
+coffee3View.setY(0);
+
+// Animate each coffee to fall downwards (to bottom of scene = 650)
+int s3 = 2;
+TranslateTransition transition1 = new TranslateTransition();
+transition1.setNode(coffee1View);
+transition1.setByY(650);  // Fall down 650 pixels
+transition1.setDuration(javafx.util.Duration.seconds(s3));
+transition1.setCycleCount(10);
+transition1.play();
+
+int s2 = 2;
+TranslateTransition transition2 = new TranslateTransition();
+transition2.setNode(coffee2View);
+transition2.setByY(650);
+transition2.setDuration(javafx.util.Duration.seconds(s2));  // Slightly slower
+transition2.play();
+int s1 = 2;
+TranslateTransition transition3 = new TranslateTransition();
+transition3.setNode(coffee3View);
+transition3.setByY(650);
+transition3.setDuration(javafx.util.Duration.seconds(s1));  // Slightly faster
+transition3.play();
+
+
     }
 }
