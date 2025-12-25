00ef9d66f9933c52acd26dc6d871676f2954fed1
Hamshik <hamshik.sbv3@gmail.com>
2025-12-25 16:13:35 +0530

    updated

--- JavaProject/source/Solution.java
+++ JavaProject/source/Solution.java
@@ -1,7 +1,17 @@
-//Used for only for challenge,if the file is unnecessary saved
-public class Solution {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
 
-        
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        BigInteger n =  in.nextBigInteger();
+        System.out.println(isPrime(n) ? "prime":"not prime");
+        in.close();
+    }
+    
+    public static boolean isPrime(BigInteger n){
+        if(n.isProbablePrime(50)) return true;
+        else return false;
     }
 }
--- JavaProject/source/StringOperations.java
+++ JavaProject/source/StringOperations.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
-public class StringOperations {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        try{
-            String a = in.nextLine();
-            String b = in.nextLine();
+void main() {
+    Scanner in = new Scanner(System.in);
+    try{
+        String a = in.nextLine();
+        String b = in.nextLine();
 
             // 1. Sum of lengths
-            System.out.println(a.length() + b.length());
+        System.out.println(a.length() + b.length());
 
             // 2. Lexicographic comparison
-            System.out.println(a.compareTo(b) > 0 ? "Yes" : "No");
+        System.out.println(a.compareTo(b) > 0 ? "Yes" : "No");
 
-            // 3. Capitalize first letters
-            String capA = a.substring(0, 1).toUpperCase() + a.substring(1);
-            String capB = b.substring(0, 1).toUpperCase() + b.substring(1);
-            System.out.println(capA + " " + capB);
-        }
-        finally{in.close();}
+        // 3. Capitalize first letters
+        String capA = a.substring(0, 1).toUpperCase() + a.substring(1);
+        String capB = b.substring(0, 1).toUpperCase() + b.substring(1);
+        System.out.println(capA + " " + capB);
     }
+    finally{in.close();}
 }
